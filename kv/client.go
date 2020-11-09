cf79a80b53702b53a34e7db308efb07ecbd30e7a
Liang Zheng <microyahoo@163.com>
2020-11-09 19:59:53 +0800

    add grpc keepalive
    Signed-off-by: Liang Zheng <microyahoo@163.com>

---
+++ kv/client.go
@@ -0,0 +1,51 @@
+// Binary client is an example client.
+package main
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	"log"
+	"time"
+
+	"google.golang.org/grpc"
+	pb "google.golang.org/grpc/examples/features/proto/echo"
+	"google.golang.org/grpc/keepalive"
+)
+
+var addr = flag.String("addr", "localhost:50052", "the address to connect to")
+
+var kacp = keepalive.ClientParameters{
+	Time:                10 * time.Second, // send pings every 10 seconds if there is no activity
+	Timeout:             time.Second,      // wait 1 second for ping ack before considering the connection dead
+	PermitWithoutStream: true,             // send pings even without active streams
+}
+
+func main() {
+	flag.Parse()
+
+	conn, err := grpc.Dial(*addr, grpc.WithInsecure(), grpc.WithKeepaliveParams(kacp))
+	if err != nil {
+		log.Fatalf("did not connect: %v", err)
+	}
+	defer conn.Close()
+
+	c := pb.NewEchoClient(conn)
+
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Minute)
+	defer cancel()
+	fmt.Println("Performing unary request")
+	ticker := time.NewTicker(time.Second)
+	defer ticker.Stop()
+	for {
+		select {
+		case <-ticker.C:
+			res, err := c.UnaryEcho(ctx, &pb.EchoRequest{Message: "keepalive demo"})
+			if err != nil {
+				log.Fatalf("unexpected error from UnaryEcho: %v", err)
+			}
+			fmt.Println("RPC response:", res)
+		}
+	}
+	// select {} // Block forever; run with GODEBUG=http2debug=2 to observe ping frames and GOAWAYs due to idleness.
+}
